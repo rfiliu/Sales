0a55f4358caf7c9805ca670d947ae4a2309df655
rfiliu <rfiliu@hotmail.com>
2019-05-04 23:35:34 -0500

    Agregando ApiService en Sales

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,49 @@
+﻿namespace Sales.Services
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using Sales.Common.Models;
+
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new System.Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+
+                if (!!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = answer,
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(answer);
+
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = list,
+                };
+
+            }
+            catch (System.Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+        }
+    }
+}
